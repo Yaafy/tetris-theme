299fef6b5bd0f1b6ee3701decb51df2076c42fce
Yaafy <132677932+Yaafy@users.noreply.github.com>
2023-05-12 16:07:10 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,69 @@
-music.setVolume(113)
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(247, music.beat(BeatFraction.Half))
-music.playTone(262, music.beat(BeatFraction.Half))
-music.playTone(294, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Half))
-music.playTone(247, music.beat(BeatFraction.Half))
-music.playTone(220, music.beat(BeatFraction.Whole))
-music.playTone(220, music.beat(BeatFraction.Half))
-music.playTone(262, music.beat(BeatFraction.Half))
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(294, music.beat(BeatFraction.Half))
-music.playTone(262, music.beat(BeatFraction.Half))
-music.playTone(247, music.beat(BeatFraction.Whole))
-music.playTone(247, music.beat(BeatFraction.Half))
-music.playTone(262, music.beat(BeatFraction.Half))
-music.playTone(294, music.beat(BeatFraction.Whole))
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(220, music.beat(BeatFraction.Whole))
-music.playTone(220, music.beat(BeatFraction.Whole))
-basic.pause(400)
-music.playTone(294, music.beat(BeatFraction.Whole))
-music.playTone(349, music.beat(BeatFraction.Half))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(392, music.beat(BeatFraction.Half))
-music.playTone(349, music.beat(BeatFraction.Half))
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(294, music.beat(BeatFraction.Half))
-music.playTone(262, music.beat(BeatFraction.Half))
-music.playTone(247, music.beat(BeatFraction.Whole))
-music.playTone(247, music.beat(BeatFraction.Half))
+basic.showLeds(`
+    # # # . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # .
+    # # # . .
+    # # . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    235
+    )
+    if (input.lightLevel() > 234) {
+        music.setVolume(113)
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(247, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(247, music.beat(BeatFraction.Half))
+        music.playTone(220, music.beat(BeatFraction.Whole))
+        music.playTone(220, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(294, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(247, music.beat(BeatFraction.Whole))
+        music.playTone(247, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(220, music.beat(BeatFraction.Whole))
+        music.playTone(220, music.beat(BeatFraction.Whole))
+        basic.pause(400)
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(294, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(247, music.beat(BeatFraction.Whole))
+        music.playTone(247, music.beat(BeatFraction.Half))
+        basic.pause(1000)
+        basic.showIcon(IconNames.Happy)
+    }
+})
